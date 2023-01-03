951246ed6965301c34c3a7926e6bacaefd2c012b
VuongDinh120 <cloudlionheat@gmail.com>
2023-01-04 00:49:54 +0700

    add customer layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,14 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   //   isAdmin = false;
   //   isPartner = false;
-  inputText = '';
+  username: string;
+  password: string;
+  fullname: string;
+  accountNum: string;
+  balance: Number;
+  dob: string;
+  address: string;
+  email: string;
+  phone: string;
 
   constructor() {
     // this.isAdmin = global.isAdmin;
     // this.isPartner = global.isPartner;
+    this.username = '';
+    this.password = '';
+    this.fullname = '';
+    this.accountNum = '';
+    this.balance = 0;
+    this.dob = '';
+    this.address='';
+    this.email='';
+    this.phone='';
   }
 
   ngOnInit(): void {
-    this.inputText = 'chao tam';
+    this.username = 'chao tam';
+    this.password = '123456';
+    this.fullname = 'Nguyễn Văn A';
+    this.accountNum = '1234578412';
+    this.balance = 15000.0;
+    this.dob = '1998-12-12';
+    this.address='365 Nguyễn Văn Trỗi Street';
+    this.email='user01@email.com';
+    this.phone='0812345678';
   }
 }
